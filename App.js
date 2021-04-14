e3d8767950906aeb13a270705b1f4d145ab0c89c
smartinsdev <smartinsdev>
2021-04-14 19:53:05 +0200

    Fix: remove Component ImageExample

--- App.js
+++ App.js
@@ -125,43 +125,6 @@ const ContactScreen = () => {
   )
 }
 
-const ImagePickerExample = () => {
-  const [image, setImage] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      if (Platform.OS !== 'web') {
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== 'granted') {
-          alert('Sorry, we need camera roll permissions to make this work!');
-        }
-      }
-    })();
-  }, []);
-
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      setImage(result.uri);
-    }
-  };
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-    </View>
-  );
-}
-
 // createStackNavigator é uma função que retorna um objeto contendo 2 propriedades:
 // Stack e Navigator. Ambos são componentes React usados ​​para configurar o navegador.
 // O Navigator deve conter elementos de Tela como seus filhos para definir a configuração das rotas.
